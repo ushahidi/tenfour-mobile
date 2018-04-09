ae773bf2d8494fcaccdb6cb4120d1121fd93d4fa
Dale Zak <dalezak@gmail.com>
2018-04-09 17:25:30 -0600

    Linked Create Organization button to website for #72

--- src/pages/signin-url/signin-url.ts
+++ src/pages/signin-url/signin-url.ts
@@ -69,7 +69,8 @@ export class SigninUrlPage extends BasePage {
 
   private createOrganization(event:any) {
     this.logger.info(this, "createOrganization");
-    this.showPage(SignupEmailPage, {});
+    this.showUrl("https://app.tenfour.org/organization/email", "_system");
+    // this.showPage(SignupEmailPage, {});
   }
 
   private showNextOnReturn(event:any) {
