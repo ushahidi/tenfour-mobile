e050bf35998503b0fc8502d4ce0113e11a2430e6
David McNamara <dmcnamara@ushahidi.com>
2018-05-24 14:34:06 +0100

    Revert api string changes

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -33,7 +33,7 @@ export class ApiProvider extends HttpProvider {
   clientSecret:string = "T7913s89oGgJ478J73MRHoO2gcRRLQ";
 
   scope:string = "user";
-  api:string = "http://api.tenfour.local";
+  api:string = "https://api.tenfour.org";
 
   constructor(
     protected device:Device,
