b3333055d79733d7d8f7812fddc5b4978deb249b
David Losada <david@tuxpiper.com>
2018-07-06 11:33:14 +0200

    chore: ushahidi/tenfour#1251 changes to deploy to production as main client

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from './environment.interface';
 
 export const Environment:IEnvironment = {
   environmentName: 'Production Environment',
-  appDomain: 'tenfour.org',
+  appDomain: 'app.tenfour.org',
   apiEndpoint: 'https://api.tenfour.org',
   clientId: "1",
   clientSecret: "T7913s89oGgJ478J73MRHoO2gcRRLQ",
