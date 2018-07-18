827a8c8cff4e406b8fe2124e0f80c85f6aea4f38
David McNamara <dmcnamara@ushahidi.com>
2018-07-18 19:26:03 +0100

    Add maxWidth, maxHeight, mimeType parameters to toThumbnailDataURL()

--- src/providers/thumbnail/thumbnail.ts
+++ src/providers/thumbnail/thumbnail.ts
@@ -3,26 +3,30 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ThumbnailProvider {
 
+  MAX_HEIGHT:number = 160;
+  MAX_WIDTH:number = 160;
+  MIME_TYPE:string = 'image/jpeg';
+
   constructor() {
   }
 
-  toThumbnailDataURL(img) {
+  toThumbnailDataURL(img, maxHeight?:number, maxWidth?:number, mimeType?:string) {
       let canvas = document.createElement('canvas');
       let ctx = canvas.getContext("2d");
       ctx.drawImage(img, 0, 0);
 
-      var MAX_WIDTH = 160;
-      var MAX_HEIGHT = 160;
+      maxHeight = maxHeight ? maxHeight : this.MAX_HEIGHT;
+      maxWidth = maxWidth ? maxWidth : this.MAX_WIDTH;
 
       if (img.width > img.height) {
-        if (img.width > MAX_WIDTH) {
-          img.height *= MAX_WIDTH / img.width;
-          img.width = MAX_WIDTH;
+        if (img.width > maxWidth) {
+          img.height *= maxWidth / img.width;
+          img.width = maxWidth;
         }
       } else {
-        if (img.height > MAX_HEIGHT) {
-          img.width *= MAX_HEIGHT / img.height;
-          img.height = MAX_HEIGHT;
+        if (img.height > maxHeight) {
+          img.width *= maxHeight / img.height;
+          img.height = maxHeight;
         }
       }
       canvas.width = img.width;
@@ -31,7 +35,7 @@ export class ThumbnailProvider {
       var ctx2 = canvas.getContext("2d");
       ctx2.drawImage(img, 0, 0, img.width, img.height);
 
-      return canvas.toDataURL("image/jpeg");
+      return canvas.toDataURL(mimeType ? mimeType : this.MIME_TYPE);
   }
 
 }
