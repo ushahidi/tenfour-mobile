e4556a68302f168cf3e103d7f8c2a2913ed71f67
Dale Zak <dalezak@gmail.com>
2018-07-25 16:07:57 -0600

    Added Intercom native plugin for ushahidi/tenfour#1255

--- src/providers/intercom/intercom.ts
+++ src/providers/intercom/intercom.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Platform } from 'ionic-angular';
 
-import { Intercom } from 'ng-intercom';
+import { Intercom as IntercomWeb } from 'ng-intercom';
+import { Intercom as IntercomNative } from '@ionic-native/intercom';
 
 import { Device } from '@ionic-native/device';
 import { AppVersion } from '@ionic-native/app-version';
@@ -21,48 +22,80 @@ export class IntercomProvider {
     private appVersion:AppVersion,
     private logger:LoggerProvider,
     private environment:EnvironmentProvider,
-    public intercom:Intercom) {
+    private intercomWeb:IntercomWeb,
+    private intercomNative:IntercomNative) {
   }
 
-  public initialize() {
-    if (!this.environment.getIntercomAppId()) {
-      return this.logger.warn(this, 'Skipping Intercom initialization - no app id.');
-    }
-
-    this.logger.info(this, 'Initializing Intercom');
-
-    require('./intercom.loader')();
-
-    this.intercom.boot({
-      app_id: this.environment.getIntercomAppId(),
-      // product: 'TenFour',
-      alignment: 'left',
-      widget: {
-        "activator": "#intercom"
+  public initialize():Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.logger.info(this, "initialize");
+      if (this.environment.getIntercomAppId()) {
+        if (this.platform.is("cordova")) {
+          this.intercomNative.setLauncherVisibility('VISIBLE').then((visible:any) => {
+            this.logger.info(this, "initialize", "setLauncherVisibility", visible);
+            resolve(true);
+          },
+          (error:any) => {
+            this.logger.error(this, "initialize", "setLauncherVisibility", error);
+            resolve(false);
+          });
+        }
+        else {
+          require('./intercom.loader')();
+          let settings = {
+            app_id: this.environment.getIntercomAppId(),
+            alignment: 'left',
+            widget: {
+              "activator": "#intercom"
+            }
+          };
+          this.logger.info(this, "initialize", "boot", settings);
+          this.intercomWeb.boot(settings);
+          resolve(true);
+        }
+      }
+      else {
+        resolve(false);
       }
     });
   }
 
-  public trackLogin(organization:Organization, user:User) {
-    if (!this.intercom || !window.hasOwnProperty('Intercom')) {
-      return this.logger.warn(this, 'trackLogin', 'intercom not initialized.');
-    }
-
-    if (organization && user) {
-      this.intercom.update({
-        name: user.name,
-        email: organization.email,
-        // phone: phone,
-        created_at: user.created_at,
-        user_id: user.id ? 'tf' + user.id : undefined,
-        company: {
-          id: organization.subdomain,
-          name: organization.name,
-          created_at: organization.created_at
-        },
-        alignment: 'left'
-      });
-    }
+  public trackLogin(organization:Organization, user:User):Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.logger.info(this, "trackLogin");
+      if (organization && user) {
+        let attributes = {
+          name: user.name,
+          email: organization.email,
+          created_at: user.created_at,
+          user_id: user.id ? 'tf' + user.id : undefined,
+          company: {
+            id: organization.subdomain,
+            name: organization.name,
+            created_at: organization.created_at
+          },
+          alignment: 'left'
+        };
+        if (this.platform.is("cordova")) {
+          this.intercomNative.updateUser(attributes).then((updated:any) => {
+            this.logger.info(this, "trackLogin", attributes, updated);
+            resolve(true);
+          },
+          (error:any) => {
+            this.logger.error(this, "trackLogin", attributes, error);
+            resolve(false);
+          });
+        }
+        else {
+          this.logger.info(this, "trackLogin", attributes);
+          this.intercomWeb.update(attributes);
+          resolve(true);
+        }
+      }
+      else {
+        resolve(false);
+      }
+    });
   }
 
 }
