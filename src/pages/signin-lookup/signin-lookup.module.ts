fbc3acb7032063e12b6b167fdd457cea2cff6003
David McNamara <dmcnamara@ushahidi.com>
2018-08-09 11:57:16 +0100

    SigninLookupModule

---
+++ src/pages/signin-lookup/signin-lookup.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+
+import { SigninLookupPage } from './signin-lookup';
+
+@NgModule({
+  declarations: [
+    SigninLookupPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SigninLookupPage),
+  ],
+  exports: [
+    SigninLookupPage
+  ],
+  entryComponents: [
+    SigninLookupPage
+  ]
+})
+export class SigninLookupModule {}
