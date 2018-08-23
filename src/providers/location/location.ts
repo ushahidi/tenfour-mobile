35037ff65c10e516c11c58c0a801cc4e892fc1bf
Dale Zak <dalezak@gmail.com>
2018-08-23 12:35:19 -0600

    Fixed LocationProvider on Android for ushahidi/tenfour-mobile#266

--- src/providers/location/location.ts
+++ src/providers/location/location.ts
@@ -88,8 +88,8 @@ export class LocationProvider {
       if (this.platform.is("cordova")) {
         this.geocoder.reverseGeocode(location.latitude, location.longitude).then((results:NativeGeocoderReverseResult[]) => {
           this.logger.info(this, "loadAddress", location, results);
-          if (results && results.length > 0) {
-            let result = results[0];
+          let result = this.getFirstResult(results);
+          if (result) {
             let address:any[] = [];
             if (result.thoroughfare) {
               address.push(result.thoroughfare);
@@ -224,6 +224,18 @@ export class LocationProvider {
         resolve(null);
       }
     });
+  }
+
+  private getFirstResult(results:any):any {
+    if (results) {
+      if (Array.isArray(results) && results.length > 0) {
+        return results[0];
+      }
+      if (typeof results === "object") {
+        return results;
+      }
+    }
+    return null;
   }
 
 }
