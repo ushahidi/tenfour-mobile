26c70dca954f04270b25f690ac2a7ec6f8c0f585
Dale Zak <dalezak@gmail.com>
2018-09-20 12:11:39 -0600

    Updated signin and signup screens for ushahidi/tenfour#1416

--- src/pages/signin/signin.module.ts
+++ src/pages/signin/signin.module.ts
@@ -5,12 +5,15 @@ import { SigninPage } from './signin';
 import { SigninUrlPage } from '../../pages/signin-url/signin-url';
 import { SigninUrlModule } from '../../pages/signin-url/signin-url.module';
 
+import { CheckinCardModule } from '../../components/checkin-card/checkin-card.module';
+
 @NgModule({
   declarations: [
     SigninPage,
   ],
   imports: [
     SigninUrlModule,
+    CheckinCardModule,
     IonicPageModule.forChild(SigninPage),
   ],
   exports: [
