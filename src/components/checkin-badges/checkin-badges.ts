7a9d44bc35eeb518d18aad4171a0939bbd9c4c17
Dale Zak <dalezak@gmail.com>
2018-10-02 13:35:19 -0600

    Updated badge width logic for ushahidi/tenfour#1435

--- src/components/checkin-badges/checkin-badges.ts
+++ src/components/checkin-badges/checkin-badges.ts
@@ -1,13 +1,9 @@
 import { Component, Input } from '@angular/core';
 
-import { PopoverController } from 'ionic-angular';
-
 import { Checkin } from '../../models/checkin';
 import { Answer } from '../../models/answer';
 import { Person } from '../../models/person';
 import { Reply } from '../../models/reply';
-
-import { CheckinPopoverComponent } from '../checkin-popover/checkin-popover';
 
 @Component({
   selector: 'checkin-badges',
@@ -24,7 +20,7 @@ export class CheckinBadgesComponent {
   RESPONSE_OTHER:string = "Other Response";
   RESPONSE_OTHER_COLOR:string = "#777777";
 
-  constructor(private popoverController:PopoverController) {}
+  constructor() {}
 
   protected getBadges():any {
     let _badges = [];
@@ -72,59 +68,10 @@ export class CheckinBadgesComponent {
         total = total + badge.number + 1;
       }
       for (let badge of _badges) {
-        badge.columns = Math.round(((badge.number + 1) / total) * 12);
+        badge.percentage = (badge.number + 1) / total;
       }
     }
     return _badges;
-  }
-
-  private showPopover(event:any, badge:any) {
-    let people = [];
-    if (badge.answer) {
-      people = this.getPeople(this.checkin.answerReplies(badge.answer))
-    }
-    else if (badge.text == this.RESPONSE_NONE) {
-      people = this.checkin.recipientsPending();
-    }
-    else if (badge.text == this.RESPONSE_OTHER) {
-      people = this.getPeople(this.checkin.otherReplies());
-    }
-    else {
-      people = [];
-    }
-    let popover = this.popoverController.create(CheckinPopoverComponent, {
-      title: badge.text,
-      people: people
-    }, {
-      showBackdrop: true,
-      enableBackdropDismiss: true
-    });
-    let ev = {
-      target : {
-        getBoundingClientRect : () => {
-          return {
-            top: event.clientY,
-            left: event.clientX
-          };
-        }
-      }
-    };
-    popover.present({
-      ev: ev
-    });
-  }
-
-  private getPeople(replies:Reply[]):Person[] {
-    let people = [];
-    for (let recipient of this.checkin.recipients) {
-      for (let reply of replies) {
-        if (recipient.user_id === reply.user_id) {
-          people.push(recipient);
-          continue;
-        }
-      }
-    }
-    return people;
   }
 
 }
